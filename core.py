9254bfd531025f48af299627321de2c063a04143
Andrew Makarov <le_shark42@hotmail.com>
2018-05-13 23:23:42 +0200

    Fix core

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ import eventlet
 def threads_list(board):
     return "https://2ch.hk/{0}/threads.json".format(board)
 
-def get_threads(board)
+def get_threads(board):
     timeout = eventlet.Timeout(10)
     try:
         threads = requests.get(threads_list(board))
