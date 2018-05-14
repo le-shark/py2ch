aa97586733eba9aa373f5bb6d4fa4f9d54bd9773
Andrew Makarov <le_shark42@hotmail.com>
2018-05-14 15:28:48 +0200

    add a method to retrieve a list of threads

--- core.py
+++ core.py
@@ -11,5 +11,24 @@ def get_threads_lite(board):
         return data.json()
     except eventlet.timeout.Timeout:
         return None
+    except ValueError:  # invalid request/404
+        return None
+    finally:
+        timeout.cancel()
+
+def thread_list(board, page = 0):
+    if page is 0:
+        page = "index"
+    return "https://2ch.hk/{0}/{1}.json".format(board, page)
+
+def get_thread_list(board, page = 0):
+    timeout = eventlet.Timeout(10)
+    try:
+        data = requests.get(thread_list(board, page))
+        return data.json()
+    except eventlet.timeout.Timeout:
+        return None
+    except ValueError:  # invalid request/404
+        return None
     finally:
         timeout.cancel()
--- test_core.py
+++ test_core.py
@@ -2,6 +2,26 @@ import core
 import pytest
 
 def test_threads_lite():
+    # valid board
     data = core.get_threads_lite("b")
     assert data["board"] == "b"
     assert len(data["threads"]) > 0
+
+    # invalid board specified
+    data = core.get_threads_lite("perkele")
+    assert not data
+
+def test_thread_list():
+    # page not stated; using the default value (0)
+    data = core.get_thread_list("b")
+    assert data["Board"] == "b"
+    assert len(data["threads"]) > 0
+
+    # page stated explicitly
+    data = core.get_thread_list("b", 1)
+    assert data["Board"] == "b"
+    assert len(data["threads"]) > 0
+
+    # page out of range
+    data = core.get_thread_list("b", 42)
+    assert not data
